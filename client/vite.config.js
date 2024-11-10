4a344c032bb65f0f1c05c72605fe0fa8ced5cc37
PRATIKKHARVI <pratikmkharvi@gmail.com>
2024-11-10 18:31:29 +0530

    complete signup functionality: video ref: 2:14:57

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',//add in the beginning each time u see api
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
